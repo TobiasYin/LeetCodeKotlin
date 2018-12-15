637c670348ba934e916c9d64871020b29610eea8
TobiasYin <yrt7296@gmail.com>
2018-12-16 00:22:40 +0800

    Update timeTest Module

--- src/answer/leetcode/TimeTest.kt
+++ src/answer/leetcode/TimeTest.kt
@@ -1,10 +1,10 @@
 package answer.leetcode
 
-fun timeTest(times:Int=100,func:()->Unit):Double{
+inline fun timeTest(times:Int=100,func:()->Unit):Double{
     val t1 = System.nanoTime()
     for(i in 1..times){
         func()
     }
     val t2 = System.nanoTime()
-    return (t2-t1).toDouble()/10e9
+    return (t2-t1).toDouble()/1000000000
 }
