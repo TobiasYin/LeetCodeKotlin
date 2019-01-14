ed2f34b23341e2a8131db4cc321a8be22ee42bfb
TobiasYin <yrt7296@gmail.com>
2019-01-14 23:40:10 +0800

    Refactor Some utils.

--- src/answer/leetcode/LeetCode203.kt
+++ src/answer/leetcode/LeetCode203.kt
@@ -12,9 +12,9 @@ class Solution203 {
     fun removeElements(head: ListNode?, `val`: Int): ListNode? {
         val dummyHead = ListNode(-1)
         dummyHead.next = head
-        var pre:ListNode? = dummyHead
-        while (pre!= null){
-            if (pre.next?.`val` == `val`){
+        var pre: ListNode? = dummyHead
+        while (pre != null) {
+            if (pre.next?.`val` == `val`) {
                 pre.next = pre.next?.next
                 continue
             }
@@ -22,14 +22,24 @@ class Solution203 {
         }
         return dummyHead.next
     }
+
+    //递归实现
+    fun removeElementSelf(head: ListNode?, `val`: Int): ListNode? {
+        return if (head?.`val` == `val`)
+            removeElementSelf(head.next, `val`)
+        else {
+            head?.next = removeElementSelf(head?.next, `val`)
+            head
+        }
+    }
 }
 
 fun main(args: Array<String>) {
     val solution203 = Solution203()
-    val list = makeLinkedList(1,2,3,6,4,3,2,6)!!
-    val result = getLinkedList(solution203.removeElements(list,6))
-    println(getLinkedList(solution203.removeElements(makeLinkedList(1,2,6,3,4,5,6),6)).joinToString(" -> "))
-    println(getLinkedList(solution203.removeElements(makeLinkedList(1,1),1)).joinToString(" -> "))
-    println(solution203.removeElements(null,3))
+    val list = makeLinkedList(1, 2, 3, 6, 4, 3, 2, 6)!!
+    val result = getLinkedList(solution203.removeElementSelf(list, 6))
+//    println(getLinkedList(solution203.removeElements(makeLinkedList(1, 2, 6, 3, 4, 5, 6), 6)).joinToString(" -> "))
+//    println(getLinkedList(solution203.removeElements(makeLinkedList(1, 1), 1)).joinToString(" -> "))
+//    println(solution203.removeElements(null, 3))
     println(result.joinToString(" -> "))
 }
