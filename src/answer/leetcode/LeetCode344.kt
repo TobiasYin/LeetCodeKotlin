c56dde04e383997e3159dd2c24637322f5c21860
TobiasYin <yrt7296@gmail.com>
2019-01-21 13:40:20 +0800

    Add Solution 344

---
+++ src/answer/leetcode/LeetCode344.kt
@@ -0,0 +1,13 @@
+package answer.leetcode
+
+class Solution344 {
+    fun reverseString(s: String): String {
+        val res = StringBuilder()
+        for(i in 1..s.length){
+            res.append(s[s.length - i])
+        }
+        return res.toString()
+    }
+//最简写法
+//    fun reverseString(s:String) = s.reversed()
+}
