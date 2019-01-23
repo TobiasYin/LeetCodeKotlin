648f5e690917cc73795c02cd851fb3023b42a7c5
TobiasYin <yrt7296@gmail.com>
2019-01-23 20:19:40 +0800

    Add Solution 211

---
+++ src/answer/leetcode/LeetCode211.kt
@@ -0,0 +1,65 @@
+package answer.leetcode
+
+
+class WordDictionary() {
+    private class Node(var isWord: Boolean = false) {
+        val next = arrayOfNulls<Node>(26)
+    }
+
+    /** Initialize your data structure here. */
+    private val root = Node()
+
+    /** Adds a word into the data structure. */
+    fun addWord(word: String) {
+        var now = root
+        for (i in word) {
+            if (now.next[i - 'a'] == null)
+                now.next[i - 'a'] = Node()
+            now = now.next[i - 'a']!!
+        }
+        now.isWord = true
+    }
+
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    fun search(word: String): Boolean {
+        return search(root, word)
+    }
+
+    private fun search(node: Node, word: String): Boolean {
+        var now = node
+        for ((index, i) in word.withIndex()) {
+            if (i != '.' && now.next[i - 'a'] == null) return false
+            else if (i == '.') {
+                return searchAll(now, word.slice(index + 1 until word.length))
+            }
+            now = now.next[i - 'a']!!
+        }
+        return now.isWord
+    }
+
+    private fun searchAll(node: Node, word: String): Boolean {
+        var result = false
+        for (i in node.next) {
+            if (i != null) {
+                result = result || search(i, word)
+            }
+        }
+        return result
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * var obj = WordDictionary()
+ * obj.addWord(word)
+ * var param_2 = obj.search(word)
+ */
+
+fun main(args: Array<String>) {
+    val wordDictionary = WordDictionary()
+    wordDictionary.addWord("bad")
+    wordDictionary.addWord("dad")
+    wordDictionary.addWord("mom")
+    println(wordDictionary.search("bad"))
+    println(wordDictionary.search(".ad"))
+}
